d1f43092af7e19543c2f4738ed0366c4686e57f6
tom <1663200+thomas-cadeau@users.noreply.github.com>
2026-03-05 18:24:15 +0100

    chapter 14 - adding authentication

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -38,7 +38,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
-            <br /> customer ID: {customerId}
           </label>
           <div className="relative">
             <select
