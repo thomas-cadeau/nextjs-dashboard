aa4b5b0da026f2d8994c48a0e7cf1f1eb456d2c4
tom <1663200+thomas-cadeau@users.noreply.github.com>
2026-03-12 12:38:15 +0100

    add user session infos

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import { PowerIcon, UserIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
+import { auth } from '@/auth';
 
-export default function SideNav() {
+export default async function SideNav() {
+  const session = await auth();
+  console.log("session: ", session);
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -18,6 +22,10 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="flex h-[48px]  grow-md items-center justify-center gap-2 w-full-md rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <UserIcon className="w-6" />
+          <div className="hidden md:block">{session?.user?.name}</div>
+        </div>
         <form action={async () => {
           'use server';
           await signOut({ redirectTo: '/' });
